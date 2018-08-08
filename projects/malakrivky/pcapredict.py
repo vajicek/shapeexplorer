96d6f19e3e23ca17f5967fa8f66202fbf027a5e3
Vaclav Krajicek <vajicek@volny.cz>
2018-08-08 08:56:26 +0200

    Extracted data from files

--- projects/malakrivky/pcapredict.py
+++ projects/malakrivky/pcapredict.py
@@ -8,21 +8,53 @@ import re
 import sys
 
 from base import rscriptsupport
-#from projects.malakrivky import io_error
+from projects.malakrivky import io_error
 
 
-SOURCE_ROOT = os.path.expanduser('~/Dropbox/krivky_mala/clanek/GRAFY/Vstupni data_ xml a jpg a txt/pracovni pro digitalizaci/M_digitalizace krivek/')
+SOURCE_ROOT = os.path.expanduser('~/DB/krivky_mala/clanek/GRAFY/Vstupni data_ xml a jpg a txt/pracovni pro digitalizaci/M_digitalizace krivek/')
+FILE_PATTERN = r"([A-Za-z\_]+)\_(.+\_.+\_.+)\.txt"
 
-def _LoadMorpho2DCurveData(input_dir, section="ac_inc_hard"):
-    for filename in glob.glob(input_dir+'/*.txt'):
+def _GetGroups(input_dir):
+    groups = {}
+    for filename in glob.glob(input_dir + '/*.txt'):
         basename = os.path.basename(filename)
-        m = re.match(r".*" + section, basename)
+        m = re.match(FILE_PATTERN, basename)
         if m:
-            print(basename)
+            groupname = m.group(2)
+            if groupname not in groups:
+                groups[groupname] = dict(count=1, files=[filename])
+            else:
+                groups[groupname]["count"] = groups[groupname]["count"] + 1
+                groups[groupname]["files"].append(filename)
+    return groups
+
+def _LoadMorpho2DCurveData(input_dir):
+    groups = _GetGroups(input_dir)
+    for groupname in groups.keys():
+        groups[groupname]["data"] = []
+        for filename in groups[groupname]["files"]:
+            coords = io_error._ExtractSemilandmarksByArcCoordinates(filename)
+            groups[groupname]["data"].append(coords)
+    return groups
+
+def _Flatten(coords):
+    return [str(coord) for lm in coords for coord in lm]
+
+def _StoreForR(output_file, groups, groupname):
+    os.makedirs(os.path.dirname(output_file), exist_ok=True)
+    with open(output_file, 'w') as file:
+        for data1 in groups[groupname]["data"]:
+            flat_coords = _Flatten(data1)
+            file.write(",".join(flat_coords) + "\n")
 
 
 #data = io_error._LoadMorpho2DCurveData(SOURCE_ROOT, io_error._ExtractSemilandmarksByArcCoordinates)
 #print(data)
 #io_error._StoreForR("soft.csv", )
 
-_LoadMorpho2DCurveData(SOURCE_ROOT)
+groups = _LoadMorpho2DCurveData(SOURCE_ROOT)
+print(groups["g_rhi_hard"]["data"])
+_StoreForR(os.path.expanduser('./hard.csv'), groups, "g_rhi_hard")
+_StoreForR(os.path.expanduser('./soft.csv'), groups, "g_rhi_soft")
+#print(groups.keys())
+#_LoadMorpho2DCurveData(SOURCE_ROOT)
