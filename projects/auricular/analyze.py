e758ce7845a257202e008cdd1d863d696b51d70f
Vaclav Krajicek <vajicek@volny.cz>
2021-02-28 14:12:49 +0100

    Report finished

--- projects/auricular/analyze.py
+++ projects/auricular/analyze.py
@@ -4,6 +4,7 @@
 
 import logging
 import pickle
+import os
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,8 +14,10 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import LeaveOneOut
 from sklearn.metrics import mean_squared_error
 
-def predictionColName(indep, dep):
-    return ("_".join(dep)) + "_by_" + ('_'.join(indep))
+import statsmodels.api as sm
+
+def predictionColName(indep, dep, type='loo'):
+    return type + '_' + ("_".join(dep)) + "_by_" + ('_'.join(indep))
 
 def oneLeaveOutPredictions(dataframe, model, indep, dep):
     loo = LeaveOneOut()
@@ -31,6 +34,24 @@ def oneLeaveOutPredictions(dataframe, model, indep, dep):
 
         dataframe.at[test_indices, prediction_column] = np.exp(prediction)
 
+def modelStatistics(X, y, indep):
+    X_with_constants = sm.add_constant(X)
+    model = sm.OLS(y, X_with_constants)
+    results = model.fit()
+    return dict(pvalue=results.pvalues[indep].values[0])
+
+def modelPredictions(dataframe, model, indep, dep):
+    prediction_column = predictionColName(indep, dep, 'train')
+    X = dataframe[indep]
+    y = dataframe[dep]
+    fit = model.fit(X, y.values.ravel())
+
+    prediction = fit.predict(dataframe[indep])
+    dataframe[prediction_column] = np.exp(prediction)
+
+    return modelStatistics(X, y, indep)
+
+
 def loadData(filename):
     return pd.read_csv(filename, sep=',', quotechar='"')
 
@@ -41,29 +62,67 @@ def addLogColumns(data, columns):
     for column_pair in columns:
         data[column_pair[1]] = np.log(data[column_pair[0]])
 
+def computeStatistics(actual, preditions):
+    mse = mean_squared_error(actual, preditions)
+    rmse = np.sqrt(mse)
+    bias = np.sum(actual - preditions) / preditions.size
+    inaccuracy = np.sum(np.abs(actual - preditions)) / preditions.size
+    return dict(rmse=rmse, bias=bias, inaccuracy=inaccuracy)
+
+
 def evaluateModel(dataframe, indep, model=LinearRegression(), dep=['logAge']):
+    model_stats = modelPredictions(dataframe, model, indep, dep)
     oneLeaveOutPredictions(dataframe, model, indep=indep, dep=dep)
+
     age_predictions = dataframe[predictionColName(indep, dep)]
     age = dataframe['age']
-    mse = mean_squared_error(age, age_predictions)
-    rmse = np.sqrt(mse)
-    return rmse
+    model_stats.update(computeStatistics(age, age_predictions))
+
+    return model_stats
 
 def evaluateAllModels(dataframe):
     indeps = [['logSAH'], ['logBE'], ['VC'], ['logSAH', 'VC'], ['logBE', 'VC']]
+    subsets = ['all']
     results = []
-    for indep in indeps:
-        print('Evaluating: logAge ~ %s' % indep)
-        rmse = evaluateModel(dataframe, indep)
-        results.append(dict(indep=indep, rmse=rmse))
+    for subset in subsets:
+        for indep in indeps:
+            print('Evaluating: logAge ~ %s' % indep)
+            model_stats = evaluateModel(dataframe, indep)
+            model_stats.update(dict(indep=indep, subset=subset))
+            results.append(model_stats)
     return results
+
+def genAgeDescriptorPlots(dataframe):
+    plots=[]
+    for x in ['BE', 'SAH', 'VC']:
+        filename = 'scatter_' + x + '.png'
+        output_filepath = os.path.join('../output/', filename)
+        plots.append({'filename': filename})
+        dataframe.plot.scatter(x=x, y='age')
+        plt.savefig(output_filepath, dpi=100)
+    return plots
+
+def genAgeHistogram(dataframe):
+    plt.figure(figsize=(10, 3))
+    dataframe['age'].hist()
+    filename = 'age_histogram.png'
+    output_filepath = os.path.join('../output/', filename)
+    plt.savefig(output_filepath, dpi=100)
+    return {'filename': filename}
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     dataframe = loadData('../output/sample_descriptors.csv')
+
     addLogColumns(dataframe, [('age', 'logAge'), ('BE', 'logBE'), ('SAH', 'logSAH')])
 
-    results = evaluateAllModels(dataframe)
+    age_histogram = genAgeHistogram(dataframe)
+    age_descriptor = genAgeDescriptorPlots(dataframe)
+    model_results = evaluateAllModels(dataframe)
+    analysis_result = dict(
+        model_results=model_results,
+        age_descriptor=age_descriptor,
+        age_histogram=age_histogram)
 
     saveData(dataframe, "../output/sample_estimates.csv")
-    pickle.dump(results, open("../output/analysis_result.pickle", 'wb'))
+    pickle.dump(analysis_result, open("../output/analysis_result.pickle", 'wb'))
--- projects/auricular/report.py
+++ projects/auricular/report.py
@@ -12,10 +12,9 @@ from jinja2 import Template
 
 from analyze import loadData
 
-def generatePdf(html, filename):
-    html = HTML(string=html, base_url='../output')
-    html.write_pdf(filename,
-        stylesheets=[CSS(string='img { width: 100px; }')])
+def generatePdf(html, filename, base_url):
+    html = HTML(string=html, base_url=base_url)
+    html.write_pdf(filename)
 
 def generateHtml(template, data):
     template = Template(template)
@@ -24,19 +23,45 @@ def generateHtml(template, data):
 def getTemplate(filename):
     return open(filename).read()
 
+class Report:
+    def __init__(self, output_dir):
+        self.output_dir = output_dir
+
+    def getOutputFile(self, filename):
+        return os.path.join(self.output_dir, filename)
+
+    def generateReport(self):
+        template_file = os.path.join(os.path.dirname(__file__), "report.jinja2")
+        template = getTemplate(template_file)
+
+        dataframe = loadData(self.getOutputFile('sample_estimates.csv'))
+
+        analysis_result = pickle.load(open(self.getOutputFile('analysis_result.pickle'), 'rb'))
+
+        now = datetime.datetime.now()
+        data = {
+            "today": now,
+            "project_name": os.path.dirname(__file__),
+            "dataframe": dataframe,
+            "sample_cols": ("name", "sex", "age", "side"),
+            "descriptors_cols": ("name", "age", "BE", "SAH", "VC"),
+            "predicted_cols": ("name", "age", "loo_logAge_by_logBE", "loo_logAge_by_logSAH", "loo_logAge_by_VC"),
+            "shortened": True,
+            "analysis_result": analysis_result,
+            "describe": {
+                "side": dataframe['side'].describe(),
+                "sex": dataframe['sex'].describe(),
+                "age": dataframe['age'].describe(),
+            }
+        }
+
+        html = generateHtml(template, data)
+
+        #open(self.getOutputFile('index.html'),'w').write(html)
+
+        generatePdf(html, self.getOutputFile('report_%s.pdf' % now.strftime("%Y%m%d")), self.output_dir)
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
-
-    template = getTemplate("projects/aurikular/report.jinja2")
-
-    dataframe = loadData("../output/sample_estimates.csv")
-    results = pickle.load(open("../output/analysis_result.pickle", 'rb'))
-
-    data = {
-        "today": datetime.datetime.now(),
-        "project_name": os.path.dirname(__file__)
-    }
-
-    html = generateHtml(template, data)
-
-    generatePdf(html, '../output/report.pdf')
+    report = Report('../output')
+    report.generateReport()
